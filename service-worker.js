7aaec73fc64f362cc70418c8294c828cee3383f4
Fabio A Rodriguez <132922493+EnchantedTable@users.noreply.github.com>
2024-10-29 06:39:58 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -40,6 +40,6 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => {
       // Si el recurso está en cache, lo devuelve; si no, lo pide a la red
       return response || fetch(event.request);
-    }).catch(() => caches.match('public/index.html')) // Si todo falla, carga la página principal
+    }).catch(() => caches.match("/public/index.html")) // Si todo falla, carga la página principal
   );
 });
